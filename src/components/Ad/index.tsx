541a0d0785b8753719397e3859768790b4d50d34
Tirivashe <shaymusts@gmail.com>
2024-03-01 15:12:02 +0200

    added trophy icon

--- src/components/Ad/index.tsx
+++ src/components/Ad/index.tsx
@@ -1,10 +1,13 @@
-import { Center, Group, Text } from "@mantine/core";
+import { Center, Group, Image, Text } from "@mantine/core";
 import styles from "./styles.module.scss";
 
 const Ad = () => {
   return (
-    <Group className={styles.root} align="center" px="lg">
-      <Text>Go Pro Upgrade Now</Text>
+    <Group className={styles.root} align="center" px="xl">
+      <Image src="/src/assets/trophy.svg" alt="trophy" />
+      <Text size="lg" color="#071D55" className={styles.text} fw={"bold"}>
+        Go Pro Upgrade Now
+      </Text>
       <Center className={styles.price}>
         <Text>$1</Text>
       </Center>
